8aa99768358c9830b2794e40a26fd48c2182607d
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-08-03 20:05:46 -0400

    Fix cjs and esm dist export.

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,16 @@
 import pkg from './package.json' assert {type: 'json'};
 
-function preserveDynamicImportPlugin() {
-  return {
-    name: 'preserve-dynamic-import',
-    renderDynamicImport() {
-      return {left: 'import(', right: ')'};
-    }
-  };
-}
-
 export default [
   {
     input: './lib/index.js',
     output: [
       {
-        file: 'dist/cjs/index.cjs',
-        format: 'cjs'
+        dir: 'dist',
+
+        format: 'cjs',
+        preserveModules: true
       }
     ],
-    plugins: [preserveDynamicImportPlugin()],
     external: Object.keys(pkg.dependencies).concat(['crypto', 'util'])
   }
 ];
